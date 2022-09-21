fb084bf0740b3658abf6bba6540a3069a2a31a0c
Flávio Félix <fau_felix30@hotmail.com>
2022-09-21 15:47:52 -0300

    Feito o exercicio de js variaveis foguete

---
+++ introducao_javascript/exercicios_01/variaveis/script.js
@@ -0,0 +1,10 @@
+// Exercicio de fixação (foguete)
+
+const myName = 'Flávio';
+const birthCity = 'Santo Amaro';
+let birthYear = 1983;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
