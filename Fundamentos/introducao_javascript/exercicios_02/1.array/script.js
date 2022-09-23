88cb169c050a6b9c867e5e2f19b0fca28d933aeb
Flávio Félix <fau_felix30@hotmail.com>
2022-09-22 23:51:59 -0300

    Começando os estudos com arrays

---
+++ Fundamentos/introducao_javascript/exercicios_02/1.array/script.js
@@ -0,0 +1,16 @@
+// Exemplo 1 array
+/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length); */
+
+// Exemplo 2 
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
