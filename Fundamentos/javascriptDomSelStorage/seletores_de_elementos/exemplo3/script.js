196bb0bc72a662dd1e51c42fe00d15acc5e6807f
Flávio Félix <fau_felix30@hotmail.com>
2022-09-28 21:30:49 -0300

    Feito o exercicio Dom

--- Fundamentos/javascriptDomSelStorage/seletores_de_elementos/exemplo3/script.js
+++ Fundamentos/javascriptDomSelStorage/seletores_de_elementos/exemplo3/script.js
@@ -1,1 +1,15 @@
-document.getElementById('header-container').style.backgroundColor = "green";
+document.getElementById('header-container').style.backgroundColor = "rgb(0, 176, 105)";
+
+document.querySelectorAll('.emergency-tasks div')[0].style.backgroundColor = "rgb(255, 159, 132)";
+document.querySelectorAll('.emergency-tasks div h3')[0].style.backgroundColor = "rgb(165, 0, 243)";
+
+document.querySelectorAll('.emergency-tasks div')[1].style.backgroundColor = "rgb(255, 159, 132)";
+document.querySelectorAll('.emergency-tasks div h3')[1].style.backgroundColor = "rgb(165, 0, 243)";
+
+document.querySelectorAll('.no-emergency-tasks div')[0].style.backgroundColor = "rgb(249, 219, 94)";
+document.querySelectorAll('.no-emergency-tasks div h3')[0].style.backgroundColor = "rgb(35, 37, 37)";
+
+document.querySelectorAll('.no-emergency-tasks div')[1].style.backgroundColor = "rgb(249, 219, 94)";
+document.querySelectorAll('.no-emergency-tasks div h3')[1].style.backgroundColor = "rgb(35, 37, 37)";
+
+document.querySelector('#footer-container').style.backgroundColor = "rgb(0, 53, 51)";
