b4397d1cbf5eeb28a106948036b26487ad58b410
Flávio Félix <fau_felix30@hotmail.com>
2022-09-29 20:09:55 -0300

    Feito o exercício foguete busca de elementos foguete

--- Fundamentos/javascriptAddRemovendoTags/buscando_elementos/exerc_1/script.js
+++ Fundamentos/javascriptAddRemovendoTags/buscando_elementos/exerc_1/script.js
@@ -8,3 +8,18 @@ pai.style.color = 'red';
 // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
 const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
 primeiroFilhoDoFilho.innerText = "Vasco campeão!";
+
+// Acesse o primeiroFilho a partir de pai.
+const primeiroFilho = pai.firstElementChild;
+
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+const primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
+
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+const textElement = elementoOndeVoceEsta.nextSibling;
+
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+
+// Agora acesse o terceiroFilho a partir de pai.
+const terceiroFilho2 = pai.previousElementSibling;
