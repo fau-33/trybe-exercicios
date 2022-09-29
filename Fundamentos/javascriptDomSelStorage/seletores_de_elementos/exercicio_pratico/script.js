09f03f2c5597e48e42f4c681381446c36990b28f
Flávio Félix <fau_felix30@hotmail.com>
2022-09-29 15:08:53 -0300

    Feito o exercício 2

--- Fundamentos/javascriptDomSelStorage/seletores_de_elementos/exercicio_pratico/script.js
+++ Fundamentos/javascriptDomSelStorage/seletores_de_elementos/exercicio_pratico/script.js
@@ -4,7 +4,6 @@
       - document.getElementsByClassName()
       - document.getElementsByTagName()
 
-      2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
       3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
       4. Crie e execute uma função que corrija o texto da tag <h1>.
       5. Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo.
@@ -18,3 +17,10 @@ function changeText(){
 
 }
 changeText();
+
+/* 2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)). */
+function changeBackgroundColorGreen(){
+  const mainContent = document.getElementsByClassName('main-content')[0];
+  mainContent.style.backgroundColor = "rgb(76,164,109)";
+}
+changeBackgroundColorGreen();
