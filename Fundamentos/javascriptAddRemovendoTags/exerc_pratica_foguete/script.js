206bd30f0939ab4f6bbdf9d2ac54aa8c31908df9
Flávio Félix <fau_felix30@hotmail.com>
2022-09-30 15:55:32 -0300

    Feito os passos 4, 5 e 6

--- Fundamentos/javascriptAddRemovendoTags/exerc_pratica_foguete/script.js
+++ Fundamentos/javascriptAddRemovendoTags/exerc_pratica_foguete/script.js
@@ -1,6 +1,6 @@
 // Adicione a tag h1 com o texto Exercício - JavaScript DOM como filho da tag body;
 const elementH1 = document.createElement('h1');// criação do elemento
-h1.innerText = "Exercício - JavaScript DOM ";// manipulação do elemento
+elementH1.innerText = "Exercício - JavaScript DOM ";// manipulação do elemento
 document.body.appendChild(elementH1);// Adicionado no body
 
 // Adicione a tag main com a classe main-content como filho da tag body;
@@ -10,5 +10,20 @@ document.body.appendChild(elementMain);
 
 // Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
 const elementSectionCenter = document.createElement('section');
-elementSection.className = "center-content";
+elementSectionCenter.className = "center-content";
 elementMain.appendChild(elementSectionCenter);
+
+// Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+const elementP = document.createElement('p');
+elementP.innerText = "xablau";
+elementSectionCenter.appendChild(elementP);
+
+// Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+const elementSectionLeft = document.createElement('section');
+elementSectionLeft.className = "left-content";
+elementMain.appendChild(elementSectionLeft);
+
+// Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+const elementSectionRight = document.createElement('section');
+elementSectionRight.className = "right-content";
+elementMain.appendChild(elementSectionRight);
