587e069a2f6a672a3a1e95ee6e8c21a91c583cbc
Flávio Félix <fau_felix30@hotmail.com>
2022-10-04 16:51:27 -0300

    Feito a função inserir frase no Dom

--- Fundamentos/javascriptWebStorage/exemplo_1/script.js
+++ Fundamentos/javascriptWebStorage/exemplo_1/script.js
@@ -3,3 +3,11 @@ const button = document.getElementById('add-button');
 const input = document.getElementById('phrases-input');
 const list = document.getElementById('phrases-list');
 
+function insertPhraseInDom(){
+  const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+  const listLength = phrasesList.lenght - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+}
