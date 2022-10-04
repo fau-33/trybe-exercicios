921393c4c57f0de9c4bb20778407224e66f64670
Flávio Félix <fau_felix30@hotmail.com>
2022-10-04 15:52:02 -0300

    Feito o session storage

--- Fundamentos/javascriptWebStorage/localStorage/script.js
+++ Fundamentos/javascriptWebStorage/localStorage/script.js
@@ -1,8 +1,8 @@
 //console.log(localStorage.length);// não possui nada salvo, então o retorno é o valor: 0
 localStorage.setItem('firstname', 'Adam');// salva uma entrada com a key = 'firstname' e value = 'Adam'
 localStorage.setItem('lastname', 'Smith');// salva uma entrada com a key = 'lastname' e value = 'Smith
-console.log(localStorage.getItem('lastname'));// retorna o valor 'Smith'
+//console.log(localStorage.getItem('lastname'));// retorna o valor 'Smith'
 localStorage.removeItem('lastname');// remove a entrada referente a key = 'lastname'
 //console.log(localStorage.length);
 localStorage.clear();// limpa todas as entradas salvas em localStorage
-console.log(localStorage.length);
+//console.log(localStorage.length);
