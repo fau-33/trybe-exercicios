859c6fde9aec9db2a82c3fbb3f80662fed72051c
Flávio Félix <fau_felix30@hotmail.com>
2022-10-04 16:14:17 -0300

    Pego os valores de id no html

---
+++ Fundamentos/javascriptWebStorage/exemplo_1/script.js
@@ -0,0 +1,5 @@
+// Exemplo com Local Storage
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
+
