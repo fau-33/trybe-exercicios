9334a861bd81ff84971d4f639ed61f3acee9bb7a
Flávio Félix <fau_felix30@hotmail.com>
2022-10-04 20:33:52 -0300

    Feito dois requisitos

--- Fundamentos/javascriptWebStorage/exercicio_pratico/script.js
+++ Fundamentos/javascriptWebStorage/exercicio_pratico/script.js
@@ -3,5 +3,21 @@ window.onload = function(){
   select.addEventListener('change', function(){
     let selected = select.selectedOptions[0];
     document.body.style.backgroundColor = selected.value;
+
+    localStorage.setItem('5.4-background-color', selected.value);
   })
+  let inputFontSize = document.querySelector('input[name = "font-size"]');
+    inputFontSize.addEventListener('change', function(){
+      let p = document.querySelector('p')
+      p.style.fontSize = `${inputFontSize.value}px`;
+
+      localStorage.setItem('5.4-font-size', `${inputFontSize.value}px`);
+    })
+
+    let saveBackground = localStorage.getItem('5.4-background-color');
+    document.body.style.backgroundColor = saveBackground;
+
+    let saveFontSize = localStorage.getItem('5.4-font-size');
+    let p = document.querySelector('p')
+    p.style.fontSize = saveFontSize;
 }
