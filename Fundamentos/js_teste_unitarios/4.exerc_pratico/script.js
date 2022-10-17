afe6104929f997337031718c1d4862db4e51376f
Flávio Félix <fau_felix30@hotmail.com>
2022-10-17 19:55:53 -0300

    Feito o exercicio prático 1

--- Fundamentos/js_teste_unitarios/4.exerc_pratico/script.js
+++ Fundamentos/js_teste_unitarios/4.exerc_pratico/script.js
@@ -7,7 +7,7 @@ Modifique as variáveis para que respeitem o escopo onde estão declaradas;
 Modifique as concatenações para template literals.
  */
 
-const testingScope = (escopo) => {
+/* const testingScope = (escopo) => {
   if(escopo === true) {
     let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
@@ -18,4 +18,33 @@ const testingScope = (escopo) => {
   }
 }
 
-testingScope(false);
+testingScope(false); */
+
+/* const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = ( ) => {
+  oddsAndEvens[0] = 2
+  oddsAndEvens[1] = 3
+  oddsAndEvens[2] = 4
+  oddsAndEvens[3] = 7
+  oddsAndEvens[4] = 10
+  oddsAndEvens[5] = 13
+  return oddsAndEvens;
+  
+}
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente.`); */
+
+/* const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortArrayBonus = (array) => {
+  const sortOddsAndEvens = array.sort((a, b) => a - b);
+  return sortOddsAndEvens;
+};
+
+const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
+console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente!`); */
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
