d840f4f6f53847d0db4975903ca62f7f9197f8e9
Flávio Félix <fau_felix30@hotmail.com>
2022-10-18 15:35:06 -0300

    Feito o exercício 2 parte 2

--- Fundamentos/js_teste_unitarios/5.exerc_pratico/script.js
+++ Fundamentos/js_teste_unitarios/5.exerc_pratico/script.js
@@ -30,3 +30,28 @@ const factorial = (n) => {
 
 const print = factorial(10);
 //console.log(`Esse é o fatorial ${print}`);
+
+/**
+ * Exercício 2
+Crie uma função que receba uma frase como parâmetro e retorne a maior palavra contida nesta frase.
+ */
+
+/* const longestWord = (text) => {
+  const wordArray = text.split(' ');
+  let maxLength = 0;
+  let result = '';
+
+  for(let word of wordArray) {
+    if(word.length > maxLength) {
+      maxLength = word.length;
+      result = word;
+    }
+  }
+  return result;
+}
+ */
+//console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'));
+
+const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+//console.log(longestWord("Antonio foi ao banheiro e não sabemos o que aconteceu"));
