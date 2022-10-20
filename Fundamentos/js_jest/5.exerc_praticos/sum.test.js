99f5baebec231d206a776775748db6e86871d09c
Flávio Félix <fau_felix30@hotmail.com>
2022-10-20 13:44:55 -0300

    Feito o exercício 1 foguete

--- Fundamentos/js_jest/5.exerc_praticos/sum.test.js
+++ Fundamentos/js_jest/5.exerc_praticos/sum.test.js
@@ -3,5 +3,11 @@ const sum = require('./sum');
 describe('Testa a função sum', () => {
   it('Deve retornar o resultado da soma', () => {
     expect(sum(4,5)).toBe(9);
-  })
-})
+  });
+  it('Deve retornar o resultado da soma zero e zero for igual a zero', () => {
+    expect(sum(0,0)).toBe(0);
+  });
+  it('Testar se a função ao lançar uma string e um numero', () => {
+    expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+  });
+});
