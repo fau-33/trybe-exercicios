05cb75e9f073e47c0825dd3b2114b85162793df5
Flávio Félix <fau_felix30@hotmail.com>
2022-10-24 19:40:27 -0300

    Feito o exercio pratico 1

---
+++ Fundamentos/js_higher_order_functions/3.exerc_pratico/script.js
@@ -0,0 +1,15 @@
+const employeeGenerator = (fullname) => {
+  const email = fullname.toLowerCase().split('').join('_');
+  return {fullname, email: `${email}@trybe.com`};
+}
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'),
+    id2: callback('Luiza Drumond'),
+    id3: callback('Carla Paiva')
+  }
+  return employees;
+}
+
+console.log(newEmployees(employeeGenerator));
