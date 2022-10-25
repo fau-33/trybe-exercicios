a2ead60bef79f28ab1d91d9bf2e2ddabdcd78885
Flávio Félix <fau_felix30@hotmail.com>
2022-10-25 11:28:52 -0300

    Feito o exercicio pratico2

--- Fundamentos/js_higher_order_functions/3.exerc_pratico/script.js
+++ Fundamentos/js_higher_order_functions/3.exerc_pratico/script.js
@@ -1,5 +1,8 @@
+/* Nova pessoa contratada
+Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id. A sua função deve receber como parâmetro o nome completo da pessoa funcionária e, a partir dele, gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
+ */
 const employeeGenerator = (fullname) => {
-  const email = fullname.toLowerCase().split('').join('_');
+const email = fullname.toLowerCase().split('').join('_');
   return {fullname, email: `${email}@trybe.com`};
 }
 
