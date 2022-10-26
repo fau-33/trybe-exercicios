9703941611a07373e462b2cbf1302202294879fb
Flávio Félix <fau_felix30@hotmail.com>
2022-10-26 15:36:04 -0300

    Feito o exercicio sobre find e sort

--- Fundamentos/js_higher_order_functions_arrays/8.exerc_praticos/data.js
+++ Fundamentos/js_higher_order_functions_arrays/8.exerc_praticos/data.js
@@ -68,7 +68,7 @@ const authorBorn1947 = () => {
 //console.log(authorBorn1947());
 
 // Retorne o nome do livro de menor nome.
-function smallerName() {
+/* function smallerName() {
   let nameBook;
   books.forEach((book) => {
     if (!nameBook || book.name.length < nameBook.length) {
@@ -76,5 +76,73 @@ function smallerName() {
     }
   });
   return nameBook;
+} */
+//console.log(smallerName());
+
+// Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+/* const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+        name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+    releaseYear: 1991,
+}; */
+function getNamedBook() {
+  return books.find((book) => book.name.length === 26);
 }
-console.log(smallerName());
+//console.log(getNamedBook());
+
+// Ordene os livros por data de lançamento em ordem decrescente.
+/* const expectedResult = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+]; */
+
+function booksOrderedByReleaseYearDesc() {
+  return books.sort((releaseA, releaseB) => releaseB.releaseYear - releaseA.releaseYear);
+}
+//console.log(booksOrderedByReleaseYearDesc());
