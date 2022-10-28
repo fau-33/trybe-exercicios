3d0146a97e4a961e077643338e45292e9746671b
Flávio Félix <fau_felix30@hotmail.com>
2022-10-27 22:12:12 -0300

    Feito o exercicio 3 pratico

---
+++ Fundamentos/js_higher_order_functions_arrays2/2.exerc_pratico/exercise3.js
@@ -0,0 +1,25 @@
+// 🚀 Exercício 3
+//Dado o array de nomes, utilize o reduce para retornar a quantidade de vezes em que aparece a letra a, maiúscula ou minúscula.
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+//const expectedResult = 20;
+
+function containsA() {
+  let totalOfLettersA = 0;
+  names.forEach((name) => {
+    const lettersFromName = name.split('');
+    totalOfLettersA += lettersFromName.reduce((lettersAInName, currentLetter) => {
+      if(currentLetter.toLowerCase() === 'a' || currentLetter.toUpperCase() === 'A'){
+        return lettersAInName + 1;
+      }
+      return lettersAInName;
+    }, 0);
+  });
+  return totalOfLettersA;
+}
+
+console.log(containsA());
