d96284c0392385812a73f34b9317fdc489fe1ebf
Flávio Félix <fau_felix30@hotmail.com>
2022-10-27 21:17:10 -0300

    Feito o exercicio 2.2 da prática

--- Fundamentos/js_higher_order_functions_arrays2/2.exerc_pratico/exercise2.js
+++ Fundamentos/js_higher_order_functions_arrays2/2.exerc_pratico/exercise2.js
@@ -1,9 +1,10 @@
+const { books } = require('./data/data');
 const data = require('./data/data');
 
 // 🚀 Exercício 2
 // Considere o seguinte array e manipule-o utilizando reduce para fazer o que é pedido:
 
-// Crie uma string com os nomes de todas as pessoas autoras.
+// 1.Crie uma string com os nomes de todas as pessoas autoras.
 /* const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
  */
 
@@ -12,6 +13,17 @@ function reduceNames() {
     return `${acc} ${curr.author.name},`;
   }, '')
 }
-console.log(reduceNames());
+//console.log(reduceNames());
 
 
+// 2.Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+//const expectedResult = 43;
+
+function averageAge() {
+  const numberOfBooks = data.books.length;
+  const sumOfAges = data.books.reduce((sum, book) => 
+  sum + (book.releaseYear - book.author.birthYear),0)
+  return sumOfAges / numberOfBooks;
+}
+
+console.log(averageAge());
