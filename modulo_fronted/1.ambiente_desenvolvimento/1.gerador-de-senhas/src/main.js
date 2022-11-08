786e23e47b7c2b32b2b68a35d52b042403ca97fc
Flávio Félix <fau_felix30@hotmail.com>
2022-11-08 20:56:10 -0300

    Feito o exercício de fixação complementar

--- modulo_fronted/1.ambiente_desenvolvimento/1.gerador-de-senhas/src/main.js
+++ modulo_fronted/1.ambiente_desenvolvimento/1.gerador-de-senhas/src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid'
 import './style.css'
+import copy from 'clipboard-copy'
 
 const passwordBtnElement = document.querySelector('button')
 const displayPasswordElement = document.querySelector('h2')
@@ -7,4 +8,8 @@ const displayPasswordElement = document.querySelector('h2')
 passwordBtnElement.addEventListener('click', () => {
   const randomPassword = nanoid()
   displayPasswordElement.innerHTML = randomPassword
+})
+displayPasswordElement.addEventListener('click', async (event) => {
+  await copy(event.target.innerHTML)
+  alert('Senha copiada!')
 })
