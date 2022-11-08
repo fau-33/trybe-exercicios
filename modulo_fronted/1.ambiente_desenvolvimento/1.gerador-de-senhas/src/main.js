371b459d44bf5dfb5a411edf3a33a27430cdce4f
Flávio Félix <fau_felix30@hotmail.com>
2022-11-08 20:23:35 -0300

    Estilizando o gerador de senhas

--- modulo_fronted/1.ambiente_desenvolvimento/1.gerador-de-senhas/src/main.js
+++ modulo_fronted/1.ambiente_desenvolvimento/1.gerador-de-senhas/src/main.js
@@ -1,9 +1,10 @@
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid'
+import './style.css'
 
-const passwordBtnElement = document.querySelector('button');
-const displayPasswordElement = document.querySelector('h2');
+const passwordBtnElement = document.querySelector('button')
+const displayPasswordElement = document.querySelector('h2')
 
 passwordBtnElement.addEventListener('click', () => {
-  const randomPassword = nanoid();
-  displayPasswordElement.innerHTML = randomPassword;
-});
+  const randomPassword = nanoid()
+  displayPasswordElement.innerHTML = randomPassword
+})
